2b03b39ae90405fed716eba91803fb461637136d
Habib Ashraf <t-habas@microsoft.com>
2015-05-05 22:41:33 -0400

    Updated copyright text.

--- Vision/Classes/View/CropViewController.h
+++ Vision/Classes/View/CropViewController.h
@@ -3,7 +3,7 @@
 //  Vision
 //
 //  Created by Habib Ashraf on 03/05/2015.
-//  Copyright (c) 2015 Microsoft. All rights reserved.
+//  Copyright (c) 2015 Habib Ashraf. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
